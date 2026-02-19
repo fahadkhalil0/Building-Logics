5b27c4ca15464fb6539531282ce1ede17f7335ef
fahadkhalil0 <fahadfiverr.0@gmail.com>
2026-02-19 15:03:06 +0500

    Added two more opr

--- basics.cpp
+++ basics.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Practice File" <<endl;
-    cout << "FOP Learning"<<endl;
+int main()
+{
+    cout << "Practice File" << endl;
+    cout << "FOP Learning" << endl;
     int a;
     bool pass = true;
     bool fail = false;
 
-    cout << "FOP COURSE IS" << pass <<endl;
-    cout << "FOP COURSE IS" << fail <<endl;
+    cout << "FOP COURSE IS" << pass << endl;
+    cout << "FOP COURSE IS" << fail << endl;
 
     // Permitive Data Types:-
-    cout << "Permitive Data Types" <<endl;
-    int b,c;
+    cout << "Permitive Data Types" << endl;
+    int b, c;
     cout << "Enter the value of a:";
     cin >> a;
     b = 10;
-    c = a+b;
-    cout << "The value of c is:" << c <<endl;
+    c = a + b;
+    cout << "The value of c is:" << c << endl;
 
     double pi = 3.145789;
     bool isEven = true;
@@ -27,6 +28,32 @@ int main(){
     string greet;
     cout << "Enter a greeting message:";
     cin >> greet;
-    cout << "You entered: " << greet <<endl;
+    cout << "You entered: " << greet << endl;
+
+    // Relational & Logical Operators:-
+    int r, l;
+    cout << "Enter two numbers:";
+    cin >> r >> l;
+
+    cout << "Relational Operators:" << endl;
+    cout << "r == l: " << (r == l) << endl;
+    cout << "r != l: " << (r != l) << endl;
+    cout << "r > l: " << (r > l) << endl;
+    cout << "r < l: " << (r < l) << endl;
+
+    cout << "Logical Operators:" << endl;
+    cout << "(r > l) && (r != l): " << ((r > l) && (r != l)) << endl;
+    cout << "(r < l) || (r == l): " << ((r < l) || (r == l)) << endl;
+
+    // Swapping two values:
+    int x, y, swap;
+    x = 200;
+    y = 300;
+    cout << "Before swapping: x = " << x << ", y = " << y << endl;
+
+    swap = x;
+    x = y;
+    y = swap;
+    cout << "After swapping: x = " << x << ", y = " << y << endl;
     return 0;
 }
