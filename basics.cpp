72b11b20a374e3d114054c99479790277a0970b8
fahadkhalil0 <fahadfiverr.0@gmail.com>
2026-02-19 14:50:46 +0500

    Building logics with understanding of basics

---
+++ basics.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Practice File" <<endl;
+    cout << "FOP Learning"<<endl;
+    int a;
+    bool pass = true;
+    bool fail = false;
+
+    cout << "FOP COURSE IS" << pass <<endl;
+    cout << "FOP COURSE IS" << fail <<endl;
+
+    // Permitive Data Types:-
+    cout << "Permitive Data Types" <<endl;
+    int b,c;
+    cout << "Enter the value of a:";
+    cin >> a;
+    b = 10;
+    c = a+b;
+    cout << "The value of c is:" << c <<endl;
+
+    double pi = 3.145789;
+    bool isEven = true;
+
+    return 0;
+}
