1441d682ec31102f4aa88fe0d08e4cdd4005e983
fahadkhalil0 <fahadfiverr.0@gmail.com>
2026-02-21 16:57:04 +0500

    Changed the formula structure in file

--- Bill_Split_System.cpp
+++ Bill_Split_System.cpp
@@ -7,19 +7,18 @@ int main(){
     cout << " Bill Split System " <<endl;
     cout << " ----------------------------" <<endl;
 
-    double totalBill;
+    int totalBill;
     cout << "Enter the total bill amount: ";
     cin >> totalBill;
-    double BS = totalBill /2;
+    // double BS = totalBill /2; ---> Manaual CHecking
 
-    if (totalBill == BS){
+    if (totalBill %2 == 0){ // Checking if the total bill is divisible by 2!
         cout << " You can split evenly. "<<endl;
     } else {
         cout << " One person pay extra bill."<<endl;
     }
 
     cout << "Your total bill is: " << totalBill <<endl;
-    cout << "Each person should pay: " << BS <<endl;
     cout << "Thank you for using our Bill Split System." <<endl;
     return 0;
 }
