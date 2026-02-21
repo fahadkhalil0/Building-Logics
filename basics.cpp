4cdb35f324d09dc34d728b14a4a54fda8d99c73f
fahadkhalil0 <fahadfiverr.0@gmail.com>
2026-02-21 16:17:04 +0500

    Working on conditional statements

--- basics.cpp
+++ basics.cpp
@@ -3,57 +3,22 @@ using namespace std;
 
 int main()
 {
-    cout << "Practice File" << endl;
-    cout << "FOP Learning" << endl;
-    int a;
-    bool pass = true;
-    bool fail = false;
+    cout << " ----------------------------" <<endl;
+    cout << " COnditional statements" <<endl;
+    cout << " ----------------------------" <<endl;
 
-    cout << "FOP COURSE IS" << pass << endl;
-    cout << "FOP COURSE IS" << fail << endl;
+    int num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
 
-    // Permitive Data Types:-
-    cout << "Permitive Data Types" << endl;
-    int b, c;
-    cout << "Enter the value of a:";
-    cin >> a;
-    b = 10;
-    c = a + b;
-    cout << "The value of c is:" << c << endl;
-
-    double pi = 3.145789;
-    bool isEven = true;
-
-    // String Operations:-
-    string greet;
-    cout << "Enter a greeting message:";
-    cin >> greet;
-    cout << "You entered: " << greet << endl;
-
-    // Relational & Logical Operators:-
-    int r, l;
-    cout << "Enter two numbers:";
-    cin >> r >> l;
-
-    cout << "Relational Operators:" << endl;
-    cout << "r == l: " << (r == l) << endl;
-    cout << "r != l: " << (r != l) << endl;
-    cout << "r > l: " << (r > l) << endl;
-    cout << "r < l: " << (r < l) << endl;
-
-    cout << "Logical Operators:" << endl;
-    cout << "(r > l) && (r != l): " << ((r > l) && (r != l)) << endl;
-    cout << "(r < l) || (r == l): " << ((r < l) || (r == l)) << endl;
-
-    // Swapping two values:
-    int x, y, swap;
-    x = 200;
-    y = 300;
-    cout << "Before swapping: x = " << x << ", y = " << y << endl;
-
-    swap = x;
-    x = y;
-    y = swap;
-    cout << "After swapping: x = " << x << ", y = " << y << endl;
+    //if else- if statement
+    if (num1> num2){
+        cout << "The first number is greater than the second number." <<endl;
+    } else if (num1 < num2){
+        cout << "The first number is less than the second number." <<endl;
+    } else {
+        cout << "The first number is equal to the second number." <<endl;
+    }
+    
     return 0;
 }
